276061c33fb043388fab243a223fa3af51feee6e
Niget-Kevin <kevin.niget@gmail.com>
2023-08-27 23:52:28 +0200

    dépot des étapes une et deux

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,16 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector(".arrow_left")
+const arrowRight = document.querySelector(".arrow_right")
+
+
+arrowLeft.addEventListener("click", () => {
+    console.log("fleche de gauche")
+})
+
+
+arrowRight.addEventListener("click", () => {
+    console.log("fleche de droite")
+})
