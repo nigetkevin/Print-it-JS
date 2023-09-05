cbf6998f5455a0806d449372f8102cdfd87007d4
Niget-Kevin <niget.web@gmail.com>
2023-09-05 08:03:42 +0200

    mise à jour du dossier avec les étapes

--- assets/script.js
+++ assets/script.js
@@ -16,11 +16,11 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
+			//  etape 1
 const arrowLeft = document.querySelector(".arrow_left")
 const arrowRight = document.querySelector(".arrow_right")
 
-
+			// etape 2
 arrowLeft.addEventListener("click", () => {
     console.log("fleche de gauche")
 })
@@ -29,3 +29,51 @@ arrowLeft.addEventListener("click", () => {
 arrowRight.addEventListener("click", () => {
     console.log("fleche de droite")
 })
+
+
+const dotsContainer = document.querySelector(".dots")
+
+console.log(dotsContainer)
+
+//  etape 3
+
+slides.forEach((slide, i) => {
+    const dot = document.createElement('div')
+    dot.classList.add('dot')
+    
+    dot.addEventListener('click', () => {
+        console.log(`Bullet point ${i + 1} clicked`)
+    })
+    
+    dotsContainer.appendChild(dot)
+})
+
+// etape 4
+
+function slide(i) {
+    const slideImage = document.querySelector('.banner-img')
+    const slideText = document.querySelector('#banner p')
+    
+    slideImage.src = "./assets/images/slideshow/" + slides[i].image
+    slideText.innerHTML = slides[i].tagLine
+}
+
+arrowLeft.addEventListener("click", function() {
+    currentI = (currentI - 1 + slides.length) % slides.length
+    activeDot(currentI)
+    slide(currentI)
+})
+
+arrowRight.addEventListener("click", function() {
+    currentI = (currentI + 1) % slides.length
+    activeDot(currentI)
+    slide(currentI)
+})
+
+let currentI = 0
+
+function activeDot(i) {
+    const dots = document.querySelectorAll('.dot')
+    dots.forEach(dot => dot.classList.remove('dot_selected'))
+    dots[i].classList.add('dot_selected')
+}
